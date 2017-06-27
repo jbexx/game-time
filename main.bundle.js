a38a9a2cccba66324aca87393dbc6dad0d164314
Jonathan Beckman <jbecks.media@gmail.com>
2017-06-27 16:10:42 -0600

    will build ship constructor

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,7 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	document.write(__webpack_require__(1));
-	document.write('It works');
+	__webpack_require__(1);
 
 /***/ }),
 /* 1 */
@@ -67,13 +66,50 @@
 /* 2 */
 /***/ (function(module, exports) {
 
-	
+	class Skeleton {
+	  constructor(x, y, width, height, shape, direction, velocity) {
+	    this.x = x;
+	    this.y = y;
+	    this.width = width;
+	    this.height = height;
+	    this.shape = shape;
+	    this.direction = direction;
+	    this.velocity = velocity;
+	  }
+
+	  draw(ctx) {
+	    ctx.fillStyle = this.shape;
+	    ctx.fillRect(this.x, this.y, this.width, this.height);
+	  }
+
+	  move(movement) {
+	    this.x = movement.x;
+	    this.y = movement.y;
+	  }
+
+	}
+
+	module.exports = Skeleton;
 
 /***/ }),
 /* 3 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	
+	var Skeleton = __webpack_require__(2);
+
+	class Ship extends Skeleton {
+	  constructor(x, y, width, height, shape, direction, veloctiy) {
+	    super(x, y, width, height, shape, direction, velocity);
+	  }
+
+	  move(movement) {
+	    super.move(movement);
+	    this.x = movement.x;
+	    this.y = movement.y;
+	  }
+	}
+
+	module.exports = Ship;
 
 /***/ }),
 /* 4 */
