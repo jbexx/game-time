5db9c2edf1d3e5844c98601fcd091074b60976f8
Christie Lynam <christielynam@gmail.com>
2017-07-05 17:33:41 -0600

    add initial test file content

---
+++ test/Alien-test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+var Alien = require('../lib/Alien.js');
+
+describe('Alien', function () {
+
+  it('should be a function', function() {
+    assert.isFunction(Alien)
+  })
+})
---
+++ test/Bullet-test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+var Bullet = require('../lib/Bullet.js');
+
+describe('Bullet', function () {
+
+  it('should be a function', function() {
+    assert.isFunction(Bullet)
+  })
+})
